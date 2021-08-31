a3c0267aa70d9c79759f39cadfe140c0a213ed60
GameStockHugo <hugo.darsonville@gmail.com>
2021-08-31 18:51:09 +0200

    first push

---
+++ Singleton.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+var totem_destruct = 0
+var stage = 0
+var ghost_dead = 0
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
